4b8ba3de31c211b466314188dbc6bd22c27e36c8
Miracle Micheal <pm9966178@gmail.com>
2024-02-13 15:58:29 +0100

    DDDDD

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const toggleBtn = document.getElementById("toggleBtn");
-const navList = document.getElementById("nav-list");
-isClicked = true;
-
-// toggleBtn.classList.toggle("active");
-toggleBtn.addEventListener("click",()=>{
-    navList.style.display="block";
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const introContent = document.querySelector('.intro-content');
+    
+    // Wait for a short delay before showing the intro content
+    setTimeout(() => {
+      introContent.style.opacity = '1';
+    }, 1000); // Adjust the delay as needed
+  });
+  
+  
+  var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Mobile App developer','Product Researcher'],
+    typeSpeed: 50,
+  });
+  
+  
+  document.getElementById('searchInput').addEventListener('input', function() {
+    var searchQuery = this.value.toLowerCase();
+    var listItems = document.querySelectorAll('#searchList li');
+  
+    listItems.forEach(function(item) {
+      var text = item.textContent.toLowerCase();
+      var isVisible = text.includes(searchQuery);
+      item.style.display = isVisible ? 'block' : 'none';
+    });
+  });
+  
